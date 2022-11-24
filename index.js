1768d205e9e799b30385c04cc887ad627c750f5e
sontaya <sontaya@aspiron.co.th>
2022-11-24 09:53:49 +0700

    update code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express')
- let {set,app,f}  = require('./middleware/initial')
+
+let {set,app,f}  = require('./middleware/initial')
 let product = require('./Route/Product')
-app.use(set)
 app.use('/Product',product)
 
--- middleware/initial.js
+++ middleware/initial.js
@@ -1,27 +1,28 @@
 
- const express = require('express')
- const app = express()
- const f = require('fs')
+const express = require('express')
+const app = express()
+const f = require('fs')
 const multer = require('multer')
 const upload = multer({ dest: '../TEST/upload/' })
 const bodyParser = require('body-parser')
- var cors = require('cors');
+var cors = require('cors');
 const { env } = require('process')
- app.use(bodyParser.json())
- app.use(bodyParser.urlencoded({ extended: false }))
- app.use(cors());
- app.use(express.json())
-let port  = 443 || process.env.port
- function set(req, res, next)  {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS')
-      res.header('Content-Type', 'application/json')
-      next()
-    }
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json())
+let port = 443 || process.env.port
 
-    app.listen(port, function () {
-      console.log(`Start Server On Port ${port}`);
-    })
+function set(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  res.header('Content-Type', 'application/json')
+  next()
+}
+app.use(set)
+app.listen(port, function () {
+  console.log(`Start Server On Port ${port}`);
+})
 
-  module.exports = {set,app,f,upload}
-  
+module.exports = { set, app, f, upload }
+
