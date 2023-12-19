855d341dde9b11137438572f448bbc6b57a849a6
sontaya <sontaya@aspiron.co.th>
2023-12-19 16:20:46 +0700

    create websocket

---
+++ Route/websocket.js
@@ -0,0 +1,23 @@
+import {WebSocketServer}from 'ws'
+
+const wss = new WebSocketServer({ port: 80 }); 
+// สร้าง websockets server ที่ port 4000
+console.log('// สร้าง websockets server ที่ port 80');
+wss.on('connection', function connection(ws) { 
+    console.log('มีการเชื่อมต่อ ');
+    
+    
+    // สร้าง connection
+  ws.on('message', function incoming(message) {
+   // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
+    console.log('received: %s', message);
+  });
+ws.on('close', function close() {
+  // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
+    console.log('disconnected');
+  });
+ws.send('init message to client');
+  // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
+});
+export default wss
+
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express'
 import bodyparser from 'body-parser'
 import user from './Route/User.js'
 import motorlog from './Route/motorlog.js'
+import './Route/websocket.js'
+
 import cors from 'cors'
 const app = express();
 app.use(cors())
@@ -12,6 +14,7 @@ app.use(express.json())
 
 app.use('/User',user)
 app.use('/log',motorlog)
-app.listen(3001,()=>{
+
+app.listen(3002,()=>{
   console.log('run at');
 })
