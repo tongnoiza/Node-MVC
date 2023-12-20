6a03cedd26a8258b600f0b317c8eadabd2c9c802
sontaya <sontaya@aspiron.co.th>
2023-12-20 17:27:11 +0700

    sdfgg

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import {WebSocketServer}from 'ws'
 import bodyparser from 'body-parser'
 import user from './Route/User.js'
 import motorlog from './Route/motorlog.js'
@@ -7,13 +8,13 @@ const app = express();
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
-const port = 443
+const port = 3001
 
 app.use('/User',user)
 app.use('/log',motorlog)
+
 app.listen(port,()=>{
   console.log(`runing at ${port}`);
 })
-
 
 
