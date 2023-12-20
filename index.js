89725c76c482d85bab0dc48ba265ab183a2baf8c
sontaya <sontaya@aspiron.co.th>
2023-12-20 11:11:33 +0700

    uuu

--- index.js
+++ index.js
@@ -1,67 +1,19 @@
 import express from 'express'
-import {WebSocketServer}from 'ws'
 import bodyparser from 'body-parser'
 import user from './Route/User.js'
 import motorlog from './Route/motorlog.js'
-// import './Route/websocket.js'
 import cors from 'cors'
 const app = express();
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
 const port = 443
-function onSocketPreError(e) {
-  console.log(e);
-}
-
-function onSocketPostError(e) {
-  console.log(e);
-}
 
 app.use('/User',user)
 app.use('/log',motorlog)
-
-const s = app.listen(port,()=>{
+app.listen(port,()=>{
   console.log(`runing at ${port}`);
 })
-
-const wss = new WebSocketServer({noServer:true ,path: "/ws",}); 
-
-s.on('upgrade', (req, socket, head) => {
-  socket.on('error', onSocketPreError);
-console.log({req});
-console.log({socket});
-console.log({head});
-  // perform auth
-  // if (!!req.headers['BadAuth']) {
-  //     socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-  //     socket.destroy();
-  //     return;
-  // }
-
-  wss.handleUpgrade(req, socket, head, (ws) => {
-      socket.removeListener('error', onSocketPreError);
-      wss.emit('connection', ws, req);
-  });
-});
-
-
-
-wss.on('connection', function connection(ws) { 
-    console.log('มีการเชื่อมต่อ ');
-    // สร้าง connection
-  ws.on('message', function incoming(message) {
-   // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
-    console.log('received: %s', message);
-    ws.send(`ข้อความที่ได้รับ ${message}`);
-  });
-ws.on('close', function close() {
-  // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
-    console.log('disconnected');
-  });
-ws.send('init message to client');
-  // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
-});
 
 
 
