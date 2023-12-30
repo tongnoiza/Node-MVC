d29bf421c703951824ca05549308264d6f03bf6a
sontaya <sontaya@aspiron.co.th>
2023-12-30 15:36:59 +0700

    update code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
-const port = 443
+const port = 3000
 
 app.use('/User',user)
 app.use('/log',motorlog)
