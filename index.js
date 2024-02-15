37cc8bdf736d9e7e02162ad5b258debb71ae812a
sontaya <sontaya@aspiron.co.th>
2024-02-15 12:20:51 +0700

    ipdate ws

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ const app = express();
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 const port = 443;
-
-const wss = new WebSocketServer({port:8080 });
-
 app.use(express.json());
+app.use("/User", user);
+app.use("/log", motorlog);
+const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocketServer({ noServer: true });
 app.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit("connection", ws, request);
   });
 });
-wss.on("connection",(ws)=>{
+wss.on("connection", function connection(ws) {
   console.log("เชื่อมต่อ");
-   ws.on('message', function message(data) {
-      console.log('received: %s', data);
-      ws.send('something');
-    });
-})
+  ws.on("error", console.error);
 
-app.use("/User", user);
-app.use("/log", motorlog);
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    ws.send("something " + data);
+  });
+});
 
 app.listen(port, () => {
   console.log(`runing at port ${port}`);
