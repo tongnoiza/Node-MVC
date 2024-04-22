95ca96ac1368cb19fc116b71e1f730b7207a8c8e
tongnoiza <72687744+tongnoiza@users.noreply.github.com>
2024-04-22 12:31:52 +0700

    Update index.js port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
-const port = 8080;
+const port = 443;
 
 app.use(express.json());
 app.use("/log", motorlog);
